39e56d08df4d87b1c45ba55ee5a9113b242a3d38
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-27 20:38:08 -0800

    worked out hash scroll in javascript for dropdown links

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-$(()=> {
-
-})
+// $(()=> {
+//
+// })
+console.log('hello');
+$(document).ready(function(){
+    $('html, body').animate({
+      scrollTop: $(window.location.hash).offset().top
+    }, 1000);
+});
