c32ef46451a41967cf52b629a9f5d7f842ff713b
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-28 17:07:15 -0800

    trying to add smooth scrolling

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 console.log('hello');
 $(document).ready(function(){
     $('html, body').animate({
-      scrollTop: $(window.location.hash).offset().top
+      scrollTop: $(window.location.hash).offset().top.scrollIntoView({
+  behavior: 'smooth'
+})
     }, 1000);
 });
