cf8c5125b78b38c79e49edb78a3c7e874a71e320
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-28 17:08:55 -0800

    same

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
 console.log('hello');
 $(document).ready(function(){
     $('html, body').animate({
-      scrollTop: $(window.location.hash).offset().top.scrollIntoView({
-  behavior: 'smooth'
-})
-    }, 1000);
+      scrollTop: $(window.location.hash).offset().top
+    }, 'slow');
 });
