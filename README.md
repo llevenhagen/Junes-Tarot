5d67d6de115f7a5d7aa4e6869ad74b60cb1b44a2
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-30 23:36:40 -0800

    figured out instafeed and worked on mobile nav bar

---
+++ README.md
@@ -0,0 +1,4 @@
+sources:
+
+instafeed / magnific popup :
+https://www.youtube.com/watch?v=wwXrP-RvoSM
--- _site/js/app.js
+++ _site/js/app.js
@@ -1,29 +1,51 @@
-
-
 // console.log('hello');
 
-  // ============ AUTOMATIC SCROLL ===========
-$(document).ready(function(){
-
-
-    // $('html, body').animate({
-    //   scrollTop: $(window.location.hash).offset().top
-    // }, 'slow');
 
 // ============ INSTAFEED JAVASCRIPT ============
-var feed = new Instafeed({
-  get: 'user',
-  userId: '327369499',
-  accessToken: '327369499.45456d4.8257602705464a11bd104aad06022598',
-  type: 'image',
-  get: 'tagged',
-  tagName: 'awesome',
-  limit: 12,
-  sortBy: 'most-recent',
-  resolution: 'standard_resolution',
-  clientId: '45456d4d07ef4cd9b7a8749b370107c2',
-  template: '<div class="gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
-  });
-  feed.run();
+$(document).ready(function() {
 
+  var feed = new Instafeed({
+    get: 'user',
+    userId: '327369499',
+    accessToken: '327369499.45456d4.8257602705464a11bd104aad06022598',
+    type: 'image',
+    limit: 12,
+    sortBy: 'most-recent',
+    resolution: 'standard_resolution',
+    clientId: '45456d4d07ef4cd9b7a8749b370107c2',
+    template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
+  });
+  if ($('#instafeed').length){
+  feed.run();
+}
+// =========== MAGNIFIC CODE FOR POP UP MODAL ON CLICK - NOT WORKING YET===
+$('.gallery').magnificPopup({
+      type: 'image',
+      // the next line tells it to target the anchor tag for what to display
+      delegate: 'a',
+      gallery: {
+          enabled: true
+      }
+  });
+  // ==============SMOOTH SCROLL ===================
+  // THE FOLLOWING CODE WAS TAKEN FROM https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+  $("a").on('click', function(event) {
+// Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 'slow', function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  // END CODE USED FROM https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+  // ======================================
 });
