83240e76cc226c4be14a948aeefea1b2130f5268
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-31 00:06:13 -0800

    trying anything i see

--- js/app.js
+++ js/app.js
@@ -7,14 +7,18 @@ $(document).ready(function() {
   var feed = new Instafeed({
     get: 'user',
     userId: '327369499',
+    // junes tarot online deployment access token:
     accessToken: '327369499.81094f6.7ab95b1f31e14797976c8069f9916ca8',
+    // local junes tarot access token:
+    // accessToken: '327369499.45456d4.8257602705464a11bd104aad06022598',
     type: 'image',
     limit: 12,
     sortBy: 'most-recent',
-    resolution: 'standard_resolution',
-    // locally:
+    resolution: 'low_resolution',
+    useHttp: true,
+    // first try client id:
     // clientId: '45456d4d07ef4cd9b7a8749b370107c2',
-    // online deployment:
+    // junes tarot online deployment client id:
     clientId: '81094f69284547778497e07c897440f9',
     template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
   });
